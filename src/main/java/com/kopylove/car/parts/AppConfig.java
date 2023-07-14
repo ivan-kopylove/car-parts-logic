ded478ccf4ee5989d97511d433df374a5eff885f
Ivan Kopylov <ivan.kopylove@gmail.com>
2023-07-14 23:19:41 +0300

    wip

--- src/main/java/com/kopylove/car/parts/AppConfig.java
+++ src/main/java/com/kopylove/car/parts/AppConfig.java
@@ -5,13 +5,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import static com.github.lazyf1sh.telegram.api.client.TelegramClient.telegramClient;
+import static com.kopylove.car.parts.Util.getTelegramApiKey;
 
 @Configuration
 public class AppConfig {
 
 	@Bean
 	public TelegramClient telegramClient() {
-		return TelegramClient.telegramClient("6381658945:AAFimovCH25AMmzqZ1UQYxanlrNEsr0bgf4");
+		return TelegramClient.telegramClient(getTelegramApiKey());
 	}
 
 
---
+++ src/main/java/com/kopylove/car/parts/Util.java
@@ -0,0 +1,19 @@
+package com.kopylove.car.parts;
+
+public class Util
+{
+    public static String getTelegramApiKey()
+    {
+        final String devKey = System.getenv("CAR_PARTS_TELEGRAM_BOT_DEV");
+        if (!devKey.isBlank())
+        {
+            return devKey;
+        }
+        final String prodKey = System.getenv("CAR_PARTS_TELEGRAM_BOT_PROD");
+        if (!prodKey.isBlank())
+        {
+            return devKey;
+        }
+        throw new RuntimeException("Invalid configuration");
+    }
+}
