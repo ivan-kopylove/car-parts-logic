da804d5b46b026f7cd5c2e6b03cf0d026455bc96
Ivan Kopylov <ivan.kopylove@gmail.com>
2023-07-25 21:11:05 +0300

    maven settings

--- src/main/java/com/github/ivan/kopylove/Util.java
+++ src/main/java/com/github/ivan/kopylove/Util.java
@@ -4,6 +4,10 @@ public class Util
 {
     public static String getTelegramApiKey()
     {
+        if ("true".equals(System.getenv("CAR_PARTS_TELEGRAM_BOT_DEV_MODE")))
+        {
+            return "devModeKey";
+        }
 
         final String devKey = System.getenv("CAR_PARTS_TELEGRAM_BOT_DEV");
         if (!devKey.isBlank())
